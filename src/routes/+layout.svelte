02831852f56513b78bc6c744aff62548e170fa92
ch33kaboo <mc.ouzane@esi-sba.dz>
2025-11-29 21:43:35 +0100

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import './layout.css';
+	import favicon from '$lib/assets/favicon.svg';
+	
+	let { children } = $props();
+  import "./layout.css";
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+{@render children()}
+
